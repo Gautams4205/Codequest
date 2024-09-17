3bb6739fc6cf90fd74ed81a8ae48a234dfd1a2a8
Gautam Singh <143217783+Gautams4205@users.noreply.github.com>
2024-09-17 10:42:46 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://codequest-x30q.onrender.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
